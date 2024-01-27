eec7918add3905c6b31ca0fb2d634211dee10c73
Ammielle WB <61096548+ammiellewb@users.noreply.github.com>
2024-01-27 16:49:14 -0500

    Update content.js

--- wi$e/scripts/content.js
+++ wi$e/scripts/content.js
@@ -1,12 +1,34 @@
-// script.js
+//Get current domain
+var domain = window.location.hostname;
+domain = domain.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+var brand = "this Company";
+var overall = "N/A";
+var myid = chrome.i18n.getMessage("@@extension_id");
+
+// Function to generate a random question
+function getRandomQuestion() {
+  const questions = [
+    "Are you making a WI$E purchase?",
+    "Do you really need this?",
+    "Will this purchase bring you joy?",
+    "Can you find a better deal elsewhere?",
+    "Is this an impulse buy?"
+  ];
+
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+}
+
+// Get a random question
+const randomQuestion = getRandomQuestion();
 
 // Display a popup with a warning message and information about the extension
 const popupHtml = `
-  <div id="wise-popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background-color: white; border: 2px solid black;">
-    <h2>WI$E Warning</h2>
+  <div id="wise-popup" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; color: white; background-color: red; border: 20px solid white; border-style: dashed; border-radius: 10px;">
+    <h1 style="font-size: 300%; text-align: center;">WI$E Warning</h1>
     <p>This is a warning from the WI$E extension:</p>
-    <p><strong>www.sephora.com</strong> may not be the wisest choice for your purchases.</p>
-    <p>Please shop responsibly and consider your financial well-being.</p>
+    <p><strong>${domain}</strong> may not be the wisest choice for your purchases. Please shop responsibly and consider your financial well-being.</p>
+    <h2 style="font-size: 200%; text-align: center;">${randomQuestion}</h2>
     <button id="close-popup">Close</button>
   </div>
 `;
@@ -21,3 +43,12 @@ if (closePopupButton) {
     document.getElementById('wise-popup').remove();
   });
 }
+
+/* 
+
+if (confirmation) {
+  alert("PROCEED WITH CAUTION.");
+} else {
+  // Redirect the user to another website
+  window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
+} */
