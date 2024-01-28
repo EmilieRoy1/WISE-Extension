c0f80cecada63f111ee8cea6de62c320ca00b818
Ishika Shali <ish2004ika@gmail.com>
2024-01-27 19:12:13 -0500

    working questions

--- wi$e/scripts/content.js
+++ wi$e/scripts/content.js
@@ -1,34 +1,14 @@
-//Get current domain
-//var domain = window.location.hostname;
-//domain = domain.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
 var brand = "this Company";
 var overall = "N/A";
 var myid = chrome.i18n.getMessage("@@extension_id");
 
-// Function to generate a random question
-/*function getRandomQuestion() {
-  const questions = [
-    "Are you making a WI$E purchase?",
-    "Do you really need this?",
-    "Will this purchase bring you joy?",
-    "Can you find a better deal elsewhere?",
-    "Is this an impulse buy?"
-  ];
-
-  const randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
-}*/
-
 const questions = [
   "Are you making a WI$E purchase?",
-  "Are you sure this is not an impulse buy?",
+  "Is this an impulse buy?",
   "Will this purchase bring you joy?",
   "Do you really need this?",
   "Are you open to a more sustainable option?"
 ];
-
-// Get a random question
-//const randomQuestion = getRandomQuestion();
 
 // Display a popup with a warning message and information about the extension
 function showPopup(index) {
@@ -95,118 +75,5 @@ function showPopup(index) {
       showPopup(index);
     });
   }
-};/* else if (continueButton) {
-    showPopup(1);
-    if(closePopupButton) {
-      showPopup(2);
-      if(closePopupButton) {
-        closePopupButton.addEventListener('click', () => {
-          document.getElementById('wise-popup').remove();
-        });
-        // Redirect the user to another website
-        window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-      } else if(continueButton) {
-          showPopup(3);
-          if(closePopupButton) {
-            closePopupButton.addEventListener('click', () => {
-              document.getElementById('wise-popup').remove();
-            });
-            // Redirect the user to another website
-            window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-          } else if(continueButton) {
-              showPopup(4);
-              if(closePopupButton) {
-                //do something
-              } else if(continueButton) {
-                continueButton.addEventListener('click', () => {
-                  document.getElementById('wise-popup').remove();
-                });
-                //redirect to local business
-                window.location.href = "";
-              } else {
-                //timer
-                showPopup(4);
-              }
-          } else {
-            //timer
-            showPopup(3);
-          }
-      } else {
-        //timer
-        showPopup(2);
-      }
-    } else if(continueButton) {
-      continueButton.addEventListener('click', () => {
-        document.getElementById('wise-popup').remove();
-      });
-      // Redirect the user to another website
-      window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-    } else {
-      //timer
-      showPopup(1);
-    }
-  } else {
-    //timer and loop back to question
-    showPopup(0);
-  }
-
-};*/
+};
 showPopup(0);
-
-/*if (closePopupButton) {
-  closePopupButton.addEventListener('click', () => {
-    document.getElementById('wise-popup').remove();
-  });
-}*/
-
-
-// Add functionality to close the popup
-/*const closePopupButton = document.getElementById('close-popup');
-const continueButton = document.getElementById('continue');
-
-if (closePopupButton) {
-  // Redirect the user to another website
-  window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-} else if (continueButton) {
-  popupHtml(1);
-  if(closePopupButton) {
-    popupHtml(2);
-    if(closePopupButton) {
-      // Redirect the user to another website
-      window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-    } else if(continueButton) {
-        popupHtml(3);
-        if(closePopupButton) {
-          // Redirect the user to another website
-          window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-        } else if(continueButton) {
-            popupHtml(4);
-            if(closePopupButton) {
-              //do something
-            } else if(continueButton) {
-              //redirect to local business
-              window.location.href = "";
-            } else {
-              //timer
-              popupHtml(4);
-            }
-        } else {
-          //timer
-          popupHtml(3);
-        }
-    } else {
-      //timer
-      popupHtml(2);
-    }
-  } else if(continueButton) {
-    // Redirect the user to another website
-    window.location.href = "https://www.tiktok.com/@sadgrlswag/video/7191631951827307822";
-  } else {
-    //timer
-    popupHtml(1);
-  }
-} else {
-  //timer and loop back to question
-  popupHtml(0);
-}
-*/
